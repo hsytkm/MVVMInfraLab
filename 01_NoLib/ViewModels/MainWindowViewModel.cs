b10ad40927053b40632206681f9cd571cf42ca83
hsytkm <t_hosoya1214@yahoo.co.jp>
2018-12-12 23:57:21 +0900

    StartUpProject

---
+++ 01_NoLib/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoLibrary.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        private static readonly string DefaultTitle = "MVVM NoLibrary";
+        
+        private string _Title = DefaultTitle;
+        public string Title
+        {
+            get => _Title;
+            private set => SetProperty(ref _Title, value);
+        }
+
+        //private int _Width;
+        //public int Width
+        //{
+        //    get => _Width;
+        //    private set => SetProperty(ref _Width, value);
+        //}
+
+        //private int _Height;
+        //public int Height
+        //{
+        //    get => _Height;
+        //    private set => SetProperty(ref _Height, value);
+        //}
+
+        public MainWindowViewModel() { }
+
+    }
+}
