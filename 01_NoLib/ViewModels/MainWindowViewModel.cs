a91e2c85f31f9c3857144f030e17f1661a3463ab
hsytkm <t_hosoya1214@yahoo.co.jp>
2018-12-13 23:45:23 +0900

    Add PropertyChangedEventListener

--- 01_NoLib/ViewModels/MainWindowViewModel.cs
+++ 01_NoLib/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.IO;
+using System.Windows.Input;
 
 namespace NoLibrary.ViewModels
 {
@@ -6,12 +8,23 @@ namespace NoLibrary.ViewModels
     {
         public string Title { get; } = "MVVM NoLibrary";
 
+        #region Input
+
         private string _InputPath = @"C:\";
         public string InputPath
         {
             get => _InputPath;
             set => SetProperty(ref _InputPath, value);
         }
+
+        private long _InputFileSize;
+        public long InputFileSize
+        {
+            get => _InputFileSize;
+            set => SetProperty(ref _InputFileSize, value);
+        }
+
+        #endregion
 
         // 表示したPathを削除
         private ICommand _ClearPathCommand;
@@ -28,7 +41,17 @@ namespace NoLibrary.ViewModels
             }
         }
 
-        public MainWindowViewModel() { }
+        public MainWindowViewModel()
+        {
+            // ◆Disposeに未対応
+            var listener = new PropertyChangedEventListener(this,
+                (_, e) => {
+                    if (e.PropertyName == nameof(InputPath))
+                    {
+                        InputFileSize = !File.Exists(InputPath) ? 0 : new FileInfo(InputPath).Length;
+                    }
+                });
+        }
 
     }
 }
---
+++ 01_NoLib/ViewModels/PropertyChangedEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NoLibrary.ViewModels
+{
+    // https://qiita.com/nossey/items/7c415799bc6fda45f94e
+    class PropertyChangedEventListener : IDisposable
+    {
+        private INotifyPropertyChanged Source;
+        private readonly PropertyChangedEventHandler Handler;
+
+        public PropertyChangedEventListener(INotifyPropertyChanged source, PropertyChangedEventHandler handler)
+        {
+            Source = source;
+            Source.PropertyChanged += Handler;
+            Handler = handler;
+        }
+
+        public void Dispose()
+        {
+            if (Source != null && Handler != null)
+                Source.PropertyChanged -= Handler;
+        }
+    }
+}
