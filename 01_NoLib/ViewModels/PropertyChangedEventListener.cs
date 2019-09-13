e62c30b0ef4065098c87e287efd4bf53dff2a316
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-09-13 22:21:10 +0900

    Fix

--- 01_NoLib/ViewModels/PropertyChangedEventListener.cs
+++ 01_NoLib/ViewModels/PropertyChangedEventListener.cs
@@ -6,14 +6,14 @@ namespace NoLibrary.ViewModels
     // https://qiita.com/nossey/items/7c415799bc6fda45f94e
     class PropertyChangedEventListener : IDisposable
     {
-        private INotifyPropertyChanged Source;
+        private readonly INotifyPropertyChanged Source;
         private readonly PropertyChangedEventHandler Handler;
 
         public PropertyChangedEventListener(INotifyPropertyChanged source, PropertyChangedEventHandler handler)
         {
             Source = source;
-            Source.PropertyChanged += Handler;
             Handler = handler;
+            Source.PropertyChanged += Handler;
         }
 
         public void Dispose()
